bb75101fefd17972bfd5e62161898e4d1e0ef08e
Pavlo Rohovyi <59741604+rohovyi@users.noreply.github.com>
2022-05-21 11:13:15 +0300

    Delete ftp.js

--- gulp/config/ftp.js
+++
@@ -1,6 +0,0 @@
-export let configFTP = {
-    host: "ftp.s59.freehost.com.ua", // FTP Host
-    user: "pavloroho", // User name
-    password: "q0H4kpL7ig", // Password
-    parallel: 5 // Number of concurrent threads
-}
